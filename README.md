75478132290f8dc1c2eef376e4f5aacd88b7bb5a
Azizbek <108106253+amazingpatch@users.noreply.github.com>
2025-04-27 21:46:13 +0000

    starting bocks

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tic-tac-toe
 Play tictactoe in your python console
+
+# Idk if I will make a tic tac toe using html5, but this is hopefully a good start.
+$1
---
+++ tic-tac-toe.py
@@ -0,0 +1,11 @@
+import random
+import numpy
+
+board = [
+    [0],[0],[0],[0],[0],[0],[0],[0],[0]
+]
+user_choice
+if __name__ == '__main__':
+    start_game()  
+
+
